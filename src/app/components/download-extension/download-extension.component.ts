6d31e2ff0ec548aa7b625b75f0d03236cd756d7f
Heder David <hedermd6@gmail.com>
2024-07-15 10:50:30 -0300

    secao de downloads completa

---
+++ src/app/components/download-extension/download-extension.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-download-extension',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './download-extension.component.html',
+  styleUrl: './download-extension.component.scss'
+})
+export class DownloadExtensionComponent {
+
+  public arrayCards = signal([
+    {
+      img: 'assets/images/logo-chrome.svg',
+      alt: 'Logo Google',
+      name: 'Chrome',
+      version: '62'
+    },
+
+    {
+      img: 'assets/images/logo-firefox.svg',
+      alt: 'Logo Firefox',
+      name: 'Firefox',
+      version: '55'
+    },
+
+    {
+      img: 'assets/images/logo-opera.svg',
+      alt: 'Logo Opera',
+      name: 'Opera',
+      version: '46'
+    },
+
+  ])
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { FeaturesComponent } from '../../components/features/features.component';
+import { DownloadExtensionComponent } from '../../components/download-extension/download-extension.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent, FeaturesComponent],
+  imports: [HeaderComponent, HeroComponent, FeaturesComponent, DownloadExtensionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
