5fa20dc30a3c093d2bf3be808c1ade14524edef8
Heder David <hedermd6@gmail.com>
2024-07-17 12:39:17 -0300

    modificacoes de localhost

--- src/app/components/newsletter/newsletter.component.ts
+++ src/app/components/newsletter/newsletter.component.ts
@@ -1,24 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-newsletter',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './newsletter.component.html',
   styleUrls: ['./newsletter.component.scss']
 })
 export class NewsletterComponent {
 
-  public email: string = '';   
+  public email: string = '';
   public emailCorrectFormat = true;
   public isInputActive = false;
 
-  constructor() {}
+  constructor(private http: HttpClient) { }
 
   onInputChange() {
-    this.emailCorrectFormat = this.email === '' ? true : this.isEmailValid() ;
+    this.emailCorrectFormat = this.email === '' ? true : this.isEmailValid();
   }
 
   isEmailValid(): boolean {
@@ -28,5 +29,25 @@ export class NewsletterComponent {
 
   activeInput() {
     this.isInputActive = true;
+  }
+
+  sendEmail() {
+    if (this.emailCorrectFormat && this.email != '') {
+      const formData = new FormData();
+      formData.append('to', this.email);
+
+      this.http.post('https://sua-aplicacao-no-railway.railway.app/sendEmail', formData, {responseType: "text"})
+        .subscribe({
+          next: (response) => {
+            console.log(response);
+          },
+          error: (error) => {
+            console.error('Erro ao enviar o email', error);
+          }
+        });
+
+    } else {
+      console.error('Formato de email inválido');
+    }
   }
 }
